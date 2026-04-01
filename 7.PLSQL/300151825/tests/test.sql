72e6f430111a3668f1c6073c1fa495002e9259d1
Freedy Ebah <freedy.ebah@yahoo.com>
2026-04-01 14:18:18 -0400

    Add tests for student insertion and log verification
    Added tests for valid and invalid student insertion, as well as a check for the number of students and logs.

--- 7.PLSQL/300151825/tests/test.sql
+++ 7.PLSQL/300151825/tests/test.sql
@@ -1,1 +1,20 @@
+-- Test insertion valide
+CALL ajouter_etudiant('Ali', 22, 'ali@email.com');
 
+-- Test insertion invalide
+DO $$
+BEGIN
+    BEGIN
+        CALL ajouter_etudiant('Bob', 15, 'bob@email.com');
+    EXCEPTION
+        WHEN others THEN
+            RAISE NOTICE 'Erreur attendue OK';
+    END;
+END;
+$$;
+
+-- Test fonction
+SELECT nombre_etudiants();
+
+-- Vérifier logs
+SELECT * FROM logs;
